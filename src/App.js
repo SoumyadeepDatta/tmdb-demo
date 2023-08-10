5422b84a3d327756c089e6a2545ad59ea93effc4
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-08-10 22:53:05 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Roles from "./pages/Roles";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 function App() {
   return (
     <BrowserRouter>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar sx={{ justifyContent: "center", color: "white" }}>
+            <Link to="/" className="link">
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                TMDB-App
+              </Typography>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+      </Box>
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
         <Route exact path="/:id">
-          <Roles/>
+          <Roles />
         </Route>
       </Switch>
     </BrowserRouter>
